79bb830f83d448314bcb1759f19f9305d9437c9c
Gonçalo Serrano <aluno25948@ipt.pt>
2025-10-31 15:25:19 +0000

    Alterar o "manifesto" com as necessidades de recursos a usar pela app

--- app/src/main/java/pt/ipt/dam2025/camerax/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam2025/camerax/MainActivity.kt
@@ -1,8 +1,12 @@
 package pt.ipt.dam2025.camerax
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pt.ipt.dam2025.camerax.databinding.ActivityMainBinding
@@ -26,8 +30,68 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.imageCaptureButton.setOnClickListener {
+        // pedir permissão para usar a câmara
+        if(allPermissonsGranted()){
+            startCamera()
+        }
+        else{
+            requestPermissions()
+        }
 
+
+        binding.imageCaptureButton.setOnClickListener {
+            takePhoto()
         }
     }
+
+    fun takePhoto(){
+
+    }
+
+    /**
+     * ask for permissions
+     */
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+
+
+
+    /**
+     * define if all permissions has been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    /**
+     * if it is necessary to ask for permissions,
+     * this will evaluate the answers provided by user
+     * and start the camera, or inform user that it can not use the camera
+     */
+    private val activityResultLauncher =
+        registerForActivityResult(  ActivityResultContracts.RequestMultiplePermissions()
+        )
+        { permissions ->
+
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                // test for all types of permissions
+                if (it.key in REQUIRED_PERMISSIONS && !it.value)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                // ver também 'Snack Bar'
+                Toast.makeText(
+                    baseContext,
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                startCamera()
+            }
+        }
 }
